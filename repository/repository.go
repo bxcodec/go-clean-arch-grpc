46b92d132b0877f37fa9c5e4e79c69262a5a639c
Iman Tumorang <bxcodec@users.noreply.github.com>
2017-08-09 11:32:52 +0700

    init

---
+++ repository/repository.go
@@ -0,0 +1,19 @@
+package repository
+
+import "github.com/bxcodec/go-clean-arch-grpc/models"
+
+type ArticleRepository interface {
+	Fetch(cursor string, num int64) ([]*models.Article, error)
+	GetByID(id int64) (*models.Article, error)
+	GetByTitle(title string) (*models.Article, error)
+	Store(a *models.Article) (int64, error)
+	Delete(id int64) (bool, error)
+}
+
+type CategoryRepository interface {
+	Fetch(cursor string, num int64) ([]*models.Category, error)
+	GetByID(id int64) (*models.Category, error)
+	GetByName(title string) (*models.Category, error)
+	Store(a *models.Category) (int64, error)
+	Delete(id int64) (bool, error)
+}
---
+++ usecase/article/articleucase_test.go
@@ -0,0 +1,115 @@
+package article_test
+
+import (
+	"errors"
+	"strconv"
+	"testing"
+
+	"github.com/bxcodec/faker"
+	"github.com/bxcodec/go-clean-arch-grpc/models"
+	"github.com/bxcodec/go-clean-arch-grpc/repository/mocks"
+	ucase "github.com/bxcodec/go-clean-arch-grpc/usecase/article"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestFetch(t *testing.T) {
+	mockArticleRepo := new(mocks.ArticleRepository)
+	var mockArticle models.Article
+	err := faker.FakeData(&mockArticle)
+	assert.NoError(t, err)
+
+	mockListArtilce := make([]*models.Article, 0)
+	mockListArtilce = append(mockListArtilce, &mockArticle)
+	mockArticleRepo.On("Fetch", mock.AnythingOfType("string"), mock.AnythingOfType("int64")).Return(mockListArtilce, nil)
+	u := ucase.NewArticleUsecase(mockArticleRepo)
+	num := int64(1)
+	cursor := "12"
+	list, nextCursor, err := u.Fetch(cursor, num)
+	cursorExpected := strconv.Itoa(int(mockArticle.ID))
+	assert.Equal(t, cursorExpected, nextCursor)
+	assert.NotEmpty(t, nextCursor)
+	assert.NoError(t, err)
+	assert.Len(t, list, len(mockListArtilce))
+
+	mockArticleRepo.AssertCalled(t, "Fetch", mock.AnythingOfType("string"), mock.AnythingOfType("int64"))
+
+}
+
+func TestFetchError(t *testing.T) {
+	mockArticleRepo := new(mocks.ArticleRepository)
+
+	mockArticleRepo.On("Fetch", mock.AnythingOfType("string"), mock.AnythingOfType("int64")).Return(nil, errors.New("Unexpexted Error"))
+	u := ucase.NewArticleUsecase(mockArticleRepo)
+	num := int64(1)
+	cursor := "12"
+	list, nextCursor, err := u.Fetch(cursor, num)
+
+	assert.Empty(t, nextCursor)
+	assert.Error(t, err)
+	assert.Len(t, list, 0)
+	mockArticleRepo.AssertCalled(t, "Fetch", mock.AnythingOfType("string"), mock.AnythingOfType("int64"))
+
+}
+
+func TestGetByID(t *testing.T) {
+	mockArticleRepo := new(mocks.ArticleRepository)
+	var mockArticle models.Article
+	err := faker.FakeData(&mockArticle)
+	assert.NoError(t, err)
+
+	mockArticleRepo.On("GetByID", mock.AnythingOfType("int64")).Return(&mockArticle, nil)
+	defer mockArticleRepo.AssertCalled(t, "GetByID", mock.AnythingOfType("int64"))
+	u := ucase.NewArticleUsecase(mockArticleRepo)
+
+	a, err := u.GetByID(mockArticle.ID)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, a)
+
+}
+
+func TestStore(t *testing.T) {
+	mockArticleRepo := new(mocks.ArticleRepository)
+	var mockArticle models.Article
+	err := faker.FakeData(&mockArticle)
+	assert.NoError(t, err)
+	//set to 0 because this is test from Client, and ID is an AutoIncreament
+	tempMockArticle := mockArticle
+	tempMockArticle.ID = 0
+
+	mockArticleRepo.On("GetByTitle", mock.AnythingOfType("string")).Return(nil, models.NewErrorNotFound())
+	mockArticleRepo.On("Store", mock.AnythingOfType("*models.Article")).Return(mockArticle.ID, nil)
+	defer mockArticleRepo.AssertCalled(t, "GetByTitle", mock.AnythingOfType("string"))
+	defer mockArticleRepo.AssertCalled(t, "Store", mock.AnythingOfType("*models.Article"))
+
+	u := ucase.NewArticleUsecase(mockArticleRepo)
+
+	a, err := u.Store(&tempMockArticle)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, a)
+	assert.Equal(t, mockArticle.Title, tempMockArticle.Title)
+
+}
+
+func TestDelete(t *testing.T) {
+	mockArticleRepo := new(mocks.ArticleRepository)
+	var mockArticle models.Article
+	err := faker.FakeData(&mockArticle)
+	assert.NoError(t, err)
+
+	mockArticleRepo.On("GetByID", mock.AnythingOfType("int64")).Return(&mockArticle, models.NewErrorNotFound())
+	defer mockArticleRepo.AssertCalled(t, "GetByID", mock.AnythingOfType("int64"))
+
+	mockArticleRepo.On("Delete", mock.AnythingOfType("int64")).Return(true, nil)
+	defer mockArticleRepo.AssertCalled(t, "Delete", mock.AnythingOfType("int64"))
+
+	u := ucase.NewArticleUsecase(mockArticleRepo)
+
+	a, err := u.Delete(mockArticle.ID)
+
+	assert.NoError(t, err)
+	assert.True(t, a)
+
+}
